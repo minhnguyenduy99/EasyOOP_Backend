3ad6b9e8c3948765d2ea8f863b69eab5d1bbfe16
Nguyen Duy Minh <minhnguyenduy99@gmail.com>
2021-05-12 21:22:50 +0700

    Fix IsPublic decorator

--- src/lib/authentication/core/guards/base-auth.guard.ts
+++ src/lib/authentication/core/guards/base-auth.guard.ts
@@ -1,14 +1,21 @@
-import { ExecutionContext, Type } from "@nestjs/common";
+import { ExecutionContext, Inject, Injectable, Type } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
 import { AuthGuard } from "@nestjs/passport";
+import { DECORATORS } from "../consts";
 import { IsRoutePublic } from "./is-route-public.interface";
 
 export function BaseAuthGuard(type: string | string[]): Type<any> {
+    @Injectable()
     class BaseLocalAuthGuard extends AuthGuard(type) implements IsRoutePublic {
         private static PUBLIC_FIELD = "isPublic";
+        @Inject(Reflector)
+        protected reflector: Reflector;
 
         isPublic(context: ExecutionContext): boolean | Promise<boolean> {
-            const req = context.switchToHttp().getRequest();
-            const isPublic = req[BaseLocalAuthGuard.PUBLIC_FIELD];
+            const isPublic = this.reflector.getAllAndOverride(
+                DECORATORS.IS_PUBLIC_DECORATOR,
+                [context.getHandler(), context.getClass()],
+            );
             return isPublic;
         }
 
