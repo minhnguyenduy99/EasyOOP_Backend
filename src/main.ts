bea6d3f0dbf7113da702efa78c400e71440e906e
lichND <lichnd99@gmail.com>
2021-06-19 21:48:52 +0700

    Update main.ts
    + Add logLevel

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,18 @@ import * as cookieParser from "cookie-parser";
 import { AppConfigService } from "./app-config.service";
 import { AppModule } from "./app.module";
 
+const LogLevel = {
+    "0": ["error", "warn", "log", "debug", "verbose"],
+    "1": ["error", "warn", "log", "debug"],
+    "2": ["error", "warn", "log"],
+    "3": ["error", "warn"],
+    "4": ["error"],
+    "5": [],
+}
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
-        logger: ["error"],
+        logger: LogLevel[process.env.LOG_LEVEL],
     });
     const appConfig = app.get(AppConfigService);
 
